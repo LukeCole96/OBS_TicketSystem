546b777d125ce756defcd62244b6bd555a60b257
Luke Cole <luke.cole@sky.uk>
2019-03-21 09:35:49 +0000

    Add other users for test cases

--- src/test/scala/users/UserSpec.scala
+++ src/test/scala/users/UserSpec.scala
@@ -8,11 +8,29 @@ class UserSpec extends FeatureSpec with Matchers with BeforeAndAfter with GivenW
     scenario("Instantiate a user") {
       When("a user is created")
       val usr = new User("Luke", "Cole", "179 middleton road")
+      val vmUser = new VenueManager("testVm", "testVm", "testRoad")
+      val agent = new Agent("testAgent", "testAgent", "testRoad")
+      val customer = new Customer("testCustomer", "testCustomer", "testRoad")
 
       Then("Verify the internals are created.")
       usr.firstName shouldBe "Luke"
       usr.secondName shouldBe "Cole"
       usr.address shouldBe "179 middleton road"
+
+      And("A venue manager is created")
+      vmUser.firstName shouldBe "testVm"
+      vmUser.secondName shouldBe "testVm"
+      vmUser.address shouldBe "testRoad"
+
+      And("A agent is created")
+      agent.firstName shouldBe "testAgent"
+      agent.secondName shouldBe "testAgent"
+      agent.address shouldBe "testRoad"
+
+      And("A customer is created")
+      customer.firstName shouldBe "testCustomer"
+      customer.secondName shouldBe "testCustomer"
+      customer.address shouldBe "testRoad"
     }
   }
 }
